d4202e687d2ca90cb757a2e1cf6403199b3a956d
Nebula <40174727+fgasdasda@users.noreply.github.com>
2022-08-20 00:11:24 +0200

    Update TEST1.sh

--- TEST1.sh
+++ TEST1.sh
@@ -8,7 +8,7 @@ cd mc
 wget -O minecraft_server.jar https://api.papermc.io/v2/projects/paper/versions/1.12.2/builds/1620/downloads/paper-1.12.2-1620.jar
 echo "eula=true" > eula.txt
 cd ..
-wget -O ngrok.zip https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm.zip
+wget -O ngrok.zip https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm64.zip
 unzip ngrok
 printf "cd mc\njava -Xmx1G -jar minecraft_server.jar nogui" > m
 chmod +x m
